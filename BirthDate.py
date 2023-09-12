d52856733c0ed2e9883f49c18df848c0a84cad82
webgova <115536574+webgova@users.noreply.github.com>
2023-09-12 12:48:58 +0200

    Add files via upload

--- BirthDate.py
+++ BirthDate.py
@@ -1,22 +1,25 @@
-# import necessary libraries
+# Import necessary libraries
 import streamlit as st
 import pandas as pd
-import datetime
+import base64
+import chardet
 
-# function to clean the date format
+# Detect encoding of the file
+def detect_encoding(file_buffer):
+    result = chardet.detect(file_buffer.read())
+    file_buffer.seek(0)
+    return result['encoding']
+
+# Function to clean the date format
 def clean_date_format(date_column):
     cleaned_dates = []
     for date in date_column:
         try:
-            cleaned_date = datetime.datetime.strptime(str(date), '%Y-%m-%d').strftime('%Y/%m/%d')
+            # Use pandas to automatically detect the date format
+            dt = pd.to_datetime(str(date))
+            cleaned_date = dt.strftime('%Y/%m/%d')
         except:
-            try:
-                cleaned_date = datetime.datetime.strptime(str(date), '%d/%m/%Y').strftime('%Y/%m/%d')
-            except:
-                try:
-                    cleaned_date = datetime.datetime.strptime(str(date), '%m/%d/%Y').strftime('%Y/%m/%d')
-                except:
-                    cleaned_date = "Invalid Date"
+            cleaned_date = "Invalid Date"
         cleaned_dates.append(cleaned_date)
     return cleaned_dates
 
@@ -27,26 +30,25 @@ def main():
     
     if uploaded_file:
         if uploaded_file.name.endswith('.csv'):
-            df = pd.read_csv(uploaded_file)
+            encoding = detect_encoding(uploaded_file)
+            df = pd.read_csv(uploaded_file, encoding=encoding,delimiter=';')
         else:
             df = pd.read_excel(uploaded_file, engine='openpyxl')
         
         st.write(df)
-        
-        col_name = st.text_input("Enter the column name with Date of Birth")
-        
+
+        # Use a selectbox for column selection
+        col_name = st.selectbox("Select the column with Date of Birth", df.columns)
+
         if col_name:
-            if col_name not in df.columns:
-                st.error("This column does not exist in the dataframe.")
-            else:
-                df[f"{col_name}_cleaned"] = clean_date_format(df[col_name])
-                st.write(df)
+            df[f"{col_name}_cleaned"] = clean_date_format(df[col_name])
+            st.write(df[[col_name,f"{col_name}_cleaned"]])
                 
-                # Download Link
-                csv = df.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-                href = f'<a href="data:file/csv;base64,{b64}" download="cleaned_file.csv">Download CSV File</a>'
-                st.markdown(href, unsafe_allow_html=True)
+            # Provide a download link for the cleaned CSV
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # Convert bytes to string
+            href = f'<a href="data:file/csv;base64,{b64}" download="cleaned_file.csv">Download Cleaned CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
